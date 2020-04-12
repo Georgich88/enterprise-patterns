3be1371cbeab63a976ed2f140c63785768aaadfa
Georgich88 <georgeisaev@gmail.com>
2020-04-12 18:57:19 +0700

    Tables fields correction.

--- transaction-script/src/main/resources/create-table-contracts.sql
+++ transaction-script/src/main/resources/create-table-contracts.sql
@@ -7,9 +7,8 @@ DROP TABLE IF EXISTS public.contracts;
 CREATE TABLE public.contracts
 (
     contract_id integer NOT NULL,
-    contract_name character varying(100) NOT NULL,
-    contract_description character varying(250) NOT NULL,
     product_id integer NOT NULL,
+    date_signed date NOT NULL,
     revenue decimal NOT NULL,
     CONSTRAINT contracts_pkey PRIMARY KEY (contract_id)
 )
