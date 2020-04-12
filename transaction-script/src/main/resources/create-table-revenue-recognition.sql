42d328a373b7211d73509a155f56757f5170828e
Georgich88 <georgeisaev@gmail.com>
2020-04-12 22:18:45 +0700

    Properties and settings fixed.

--- transaction-script/src/main/resources/create-table-revenue-recognition.sql
+++ transaction-script/src/main/resources/create-table-revenue-recognition.sql
@@ -10,16 +10,16 @@ CREATE SEQUENCE revenue_recognition_id_seq;
 
 CREATE TABLE public.students
 (
-    revenue_recognition_id integer NOT NULL DEFAULT nextval('students_id_seq'),
+    revenue_recognition_id integer NOT NULL DEFAULT nextval('revenue_recognition_id_seq'),
     contract_id integer,
     date_recognition date NOT NULL,
     amount decimal NOT NULL,
-    CONSTRAINT students_pkey PRIMARY KEY (student_id)
+    CONSTRAINT revenue_recognition_pkey PRIMARY KEY (revenue_recognition_id)
 )
 WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
 
-ALTER TABLE public.students
+ALTER TABLE public.revenue_recognition
     OWNER to transaction_script;
