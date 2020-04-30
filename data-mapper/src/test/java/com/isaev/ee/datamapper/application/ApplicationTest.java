d2edba8f01912d6563e3fef85491f79f31b4d34d
Georgich88 <georgeisaev@gmail.com>
2020-04-30 14:32:58 +0700

    #5 Add JUnit tests.

---
+++ data-mapper/src/test/java/com/isaev/ee/datamapper/application/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.isaev.ee.datamapper.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+    @Test
+    public void shouldInitiateDatabase() {
+        assertDoesNotThrow(() -> Application.initiateDatabase());
+    }
+
+    @Test
+    public void shouldGenerateData() {
+        assertDoesNotThrow(() -> Application.generateData());
+    }
+}
