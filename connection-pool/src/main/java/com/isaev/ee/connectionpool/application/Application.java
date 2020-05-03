7698be40a6676630095600a7bc83b38399c784b4
Georgich88 <georgeisaev@gmail.com>
2020-05-03 13:07:52 +0700

    #7 Demo application.

---
+++ connection-pool/src/main/java/com/isaev/ee/connectionpool/application/Application.java
@@ -0,0 +1,54 @@
+package com.isaev.ee.connectionpool.application;
+
+import com.isaev.ee.connectionpool.connectionfactory.DriverManagerConnectionFactory;
+import com.isaev.ee.connectionpool.connectionpool.ConnectionPool;
+import com.isaev.ee.connectionpool.pool.PooledResourceFactory;
+
+import java.sql.Connection;
+
+/**
+ * Demo application demonstrates connection pool implementation.
+ *
+ * @author Georgy Isaev
+ */
+public class Application {
+
+    private static final String JDBC_DRIVER = "org.postgresql.Driver";
+    private static final String JDBC_DB_URL = "jdbc:postgresql://localhost:5432/data_mapper";
+    private static final String JDBC_USER = "data_mapper";
+    private static final String JDBC_PASS = "123456";
+
+    private static ConnectionPool<Connection> connectionPool = null;
+
+    public static void setupPool() throws Exception {
+
+        Class.forName(JDBC_DRIVER);
+        PooledResourceFactory factory = new DriverManagerConnectionFactory(JDBC_DB_URL, JDBC_USER, JDBC_PASS);
+        connectionPool = new ConnectionPool<Connection>(factory);
+        connectionPool.setMaxTotal(10);
+
+    }
+
+    public static ConnectionPool getConnectionPool() {
+        return connectionPool;
+    }
+
+    private static void printDbStatus() {
+        System.out.println("Max.: " + getConnectionPool().getMaxTotal()
+                + "; Active: " + getConnectionPool().getActiveNumber()
+                + "; Idle: " + getConnectionPool().getIdleNumber());
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        setupPool();
+        printDbStatus();
+        Connection connection = connectionPool.borrowResource();
+        printDbStatus();
+        connectionPool.returnResource(connection);
+        printDbStatus();
+        connectionPool.clear();
+        printDbStatus();
+
+    }
+}
